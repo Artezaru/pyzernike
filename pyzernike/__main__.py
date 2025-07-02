67a542eefbb65c18277c961258799a2162e7a516
Artezaru <artezaru.github@proton.me>
2025-07-02 17:54:46 +0200

    Add flags -r and -n for the command pyzernike

--- pyzernike/__main__.py
+++ pyzernike/__main__.py
@@ -1,4 +1,5 @@
-from pyzernike import zernike_display
+from pyzernike import zernike_display, radial_display
+import argparse
 
 def __main__() -> None:
     r"""
@@ -8,16 +9,52 @@ def __main__() -> None:
 
     .. code-block:: console
         pyzernike
+
+    This will display a set of Zernike polynomials in an interactive matplotlib figure.
+
+    .. code-block:: console
+
+        pyzernike -r -n 3
+    
+    - flag `-r` or `--radial` will display the radial Zernike polynomials instead of the full Zernike polynomials.
+    - flag `-n` or `--n` will specify the maximum order of the Zernike polynomials to display. If not specified, the default value is 5
         
     """
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Display Zernike polynomials.")
+    parser.add_argument(
+        '-r', '--radial', action='store_true',
+        help="Display radial Zernike polynomials instead of full Zernike polynomials."
+    )
+    parser.add_argument(
+        '-n', '--n', type=int, default=5,
+        help="Maximum order of the Zernike polynomials to display (default: 5)."
+    )
+
+    args = parser.parse_args()
+
+    Nzer = args.n
     list_n = []
     list_m = []
-    for n in range(0, 5):
-        for m in range(-n, n + 1, 2):
-            list_n.append(n)
-            list_m.append(m)
+    if args.radial:
+        # For radial Zernike polynomials, m is always even
+        for n in range(0, Nzer + 1):
+            for m in range(0 if n%2 == 0 else 1, n + 1, 2):
+                list_n.append(n)
+                list_m.append(m)
     
-    zernike_display(n=list_n, m=list_m)
+    else:
+        # For full Zernike polynomials, m can be both even and odd
+        for n in range(0, Nzer + 1):
+            for m in range(-n, n + 1, 2):
+                list_n.append(n)
+                list_m.append(m)
+    
+    # Display the Zernike polynomials
+    if args.radial:
+        radial_display(n=list_n, m=list_m)
+    else:
+        zernike_display(n=list_n, m=list_m)
 
 def __main_gui__() -> None:
     r"""
